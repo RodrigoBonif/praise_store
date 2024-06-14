c1e8e7d34ccd80530c7360ab429691f1acc58c70
Vinícius Clemente Negherbon <139130543+nnegherbon@users.noreply.github.com>
2024-06-14 16:51:16 -0300

    Home praticamente pronta falta apenas a linha do filtro. Adição de dois arquivos (imgs) e alterações nos documentos de HTML e CSS para finalizar a Home
    ps: PR feito apenas para subir os aquivos para a main

--- Pages/Home-marcas/Home-class/Class.js
+++ Pages/Home-marcas/Home-class/Class.js
@@ -2,7 +2,6 @@ document.addEventListener('DOMContentLoaded', () => {
     const botaoFiltro = document.querySelector('.select-filter');
 
     botaoFiltro.addEventListener('click', () => {
-
         const containerFiltro = document.querySelector('.select-container');
         const containerTitulo = document.querySelector('.select-container-titulo');
         const containerTexto = document.querySelector('.select-container-texto');
@@ -17,5 +16,20 @@ document.addEventListener('DOMContentLoaded', () => {
         } else {
             botaoFiltro.textContent = 'Selecionar filtro ↓';
         }
+    });
+
+    const checkboxBotao = document.querySelectorAll('.checkbox');
+
+    checkboxBotao.forEach(button => {
+        button.addEventListener('click', (event) => {
+            const img = button.querySelector('img');
+            const isChecked = img.src.includes('/Images/checkboxAberto.png');
+
+            if (isChecked) {
+                img.src = '/Images/checkboxFechado.png';
+            } else {
+                img.src = '/Images/checkboxAberto.png';
+            }
+        });
     });
 });
